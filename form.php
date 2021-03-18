c477c3e006cb6d466ecc6e575444e9755dd6c77f
EHatt <56519923+EHatt@users.noreply.github.com>
2021-03-18 17:53:56 +0000

    Add files via upload

---
+++ form.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+if($_POST['Spiral'] == 1 & $_POST['Ground']  == 1){
+    header("Location:'https://ehatt.github.io/WH_G_S.html");
+}
+else {
+    header("'https://ehatt.github.io/WH.html");
+}
