2f3633b5cc2c5712db9621184deea757f9cb30fc
EHatt <56519923+EHatt@users.noreply.github.com>
2021-03-18 16:48:06 +0000

    Add files via upload

---
+++ assets/js/form.js
@@ -0,0 +1,15 @@
+/*
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+function redirect()
+{ if (document.getElementById("Spiral").checked == true)
+    window.location = 'https://ehatt.github.io/index.html';
+else if (document.getElementById("Ground").checked == true)
+    window.location = 'https://ehatt.github.io/index.html';
+else if (document.getElementById("WH").checked == true)
+    window.location = 'https://ehatt.github.io/index.html';
+
+}
