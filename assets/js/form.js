632421cad2b344cca072e1d3d38734af7340577b
EHatt <56519923+EHatt@users.noreply.github.com>
2021-03-18 17:20:01 +0000

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -7,8 +7,7 @@
 function redirect()
 { if (document.getElementById("Spiral").checked == true & document.getElementById("Ground").checked == true)
     window.location = 'https://ehatt.github.io/WH_G_S.html';
-else if (document.getElementById("Ground").checked == true)
-    window.location = 'https://ehatt.github.io/index.html';
+
 else if (document.getElementById("WH").checked == true)
     window.location = 'https://ehatt.github.io/index.html';
 
